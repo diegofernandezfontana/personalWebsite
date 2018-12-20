7ae06f478eaf37cb09631a4fbc5761efada0c187
diegofernandezfontana <diegofernandezfontana@gmail.com>
2018-12-20 05:40:44 -0300

    Initial commit, setting up webpack, react, redux, node and express.

---
+++ front/src/containers/Main.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import MainTitle from './MainTitle/MainTitle'
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <MainTitle/>
+            </div>
+        );
+    }
+}
+
+export default Main;
---
+++ front/src/containers/MainTitle/MainTitle.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MainTitle extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Este es el Main Title</h1>
+            </div>
+        );
+    }
+}
+
+export default MainTitle;
